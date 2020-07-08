ff776b4fffbd4ed8352808ce35c44faab7416caf
yangyang <912962140@qq.com>
2020-07-08 19:32:58 +0800

    chapter_2

---
+++ cmake-build-debug/CMakeFiles/C_Primer_Plus_6th_answer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_Primer_Plus_6th_answer.dir/chapter_2/1.c.o"
+  "CMakeFiles/C_Primer_Plus_6th_answer.dir/main.c.o"
+  "C_Primer_Plus_6th_answer"
+  "C_Primer_Plus_6th_answer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_Primer_Plus_6th_answer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/C_Primer_Plus_6th_answer.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for C_Primer_Plus_6th_answer.
+# This may be replaced when dependencies are built.
